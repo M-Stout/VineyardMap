c85289fd99cf3620da1ee33535e95049281d8605
grimer30 <mstout64@gmail.com>
2015-04-05 22:27:27 +0100

    First Commit
    05/04/2015: Includes experimental and main branches.

---
+++ occupantData.js
@@ -0,0 +1,22 @@
+var occupantData = {
+  "P324": "grimer30",
+  "P40": "Science_Friction",
+  "P56": "seiaeka",
+  "P151": "LilyanaLion",
+  "P113": "tonycakes",
+  "P381": "sobeforestep",
+  "P188": "SharkCookieLion",
+  "P643": "Jochem2020",
+  "P304": "spaaaaacekitten",
+  "Vineyard Town Hall": "SalamanderSoul",
+  "P219": "Mouseshy",
+  "P468": "i8pichu",
+  "P171": "Dohrito",
+  "P365": "Dorno",
+  "P138": "FlintlockHS, SaltyNaCl",
+  "P387": "Nasch",
+  "P215": "sithlordRED",
+  "P509": "_Latias",
+
+
+};
---
+++ titleData.js
@@ -0,0 +1,12 @@
+var titleData= {
+  "P40": "The Night Light Club",
+  "P56": "Dragoon Lagoon Embassy",
+  "P151": "Catlantis",
+  "P113": "Catlantis",
+  "P304": "Catlantis Cafe",
+  "Vineyard Town Hall": "Vineyard Town Hall",
+  "P468": "Vineyard Science Center",
+  "P138": "Poor Peasants Cafe and Dong Dank Tour Guides - Vineyard",
+  "P215": "Justin's Secret Restaurant",
+  "P509": "Autograph Museum",
+};
