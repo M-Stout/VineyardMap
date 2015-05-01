1d73811b71fd336ac2c4899184d760c41b6155ff
grimer30 <mstout64@gmail.com>
2015-05-01 18:28:27 +0100

    Changed census form to a sliding panel
    The idea being for it not to obstruct the map for people who aren't
    using it.

---
+++ slidingForm.js
@@ -0,0 +1,26 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.form').animate({
+      right: "0"
+    }, 200);
+
+    $('body').animate({
+      right: "35%"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.form').animate({
+      right: "-35%"
+    }, 200);
+
+    $('body').animate({
+      right: "35%"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
