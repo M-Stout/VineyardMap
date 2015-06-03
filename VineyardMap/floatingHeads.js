42286328e28c3bb4e5317b1c1487a70b49c49d2d
grimer30 <mstout64@gmail.com>
2015-06-03 20:19:14 +0100

    First try at multi-directional head travel functionality
    This title is pretty cool

--- VineyardMap/floatingHeads.js
+++ VineyardMap/floatingHeads.js
@@ -1,6 +1,8 @@
 var images = ['winebarrelrocketpixelly.png', 'scifri swadloon.png', 'sciencefriction.png', 'sciencefriction.png', 'salamandersoul.png', 'dappercoat.png', 'pichu.png', 'brianguitarpixel.png', 'spacecore.png', 'spacecore.png']
 var audios = ['rocket', 'swadloon', 'sciencegold', 'sciencebible', 'salamander', 'dapperdankmemes', 'pichuwaffles', 'musicmess', 'spaceshort', 'spacelong']
+var direction = ['0', '0', '0', '0', '0', '1', '0', '0', '0', '0']
 
+var randomIndex = 0;
 
 $(document).ready(function() {
   
@@ -20,23 +22,52 @@ $(document).ready(function() {
 
 function floatBy () {
   
-  $("#floatingVoice")[0].play();
-  $( "#floatingHead" ).animate({
-    right: "105%",
-    transform: 'rotate(160deg)'
-  }, 5000, function() {
+  //right to left or left to right
+  if (direction[randomIndex] == '1') {
     
-   // Animation Complete
     $( "#floatingHead" ).css({
-      'right': "-10%",
-      'top': Math.random() * 80 + 10 +'%',
-      transform: 'rotate(0deg)'
+        'right': "110%",
     });
+    $("#floatingVoice")[0].play();
+    $( "#floatingHead" ).animate({
+      right: "-10%",
+      transform: 'rotate(160deg)'
+    }, 5000, function() {
+      
+     // Animation Complete
+      $( "#floatingHead" ).css({
+        'top': Math.random() * 80 + 10 +'%',
+        transform: 'rotate(0deg)'
+      });
+      
+      var randomIndex = Math.floor(Math.random() * images.length);
+      $("#floatingHead").attr("src","images/" + images[randomIndex]);
+      $("#floatingVoice").attr("src","audio/" + audios[randomIndex] + ".mp3");
+    });
+  } else {
     
-    var randomIndex = Math.floor(Math.random() * images.length);
-    $("#floatingHead").attr("src","images/" + images[randomIndex]);
-    $("#floatingVoice").attr("src","audio/" + audios[randomIndex] + ".mp3");
-  });
+      $( "#floatingHead" ).css({
+          'right': "-10%",
+      });    
+      $("#floatingVoice")[0].play();
+      $( "#floatingHead" ).animate({
+      right: "110%",
+      transform: 'rotate(160deg)'
+      }, 5000, function() {
+      
+     // Animation Complete
+      $( "#floatingHead" ).css({
+        'top': Math.random() * 80 + 10 +'%',
+        transform: 'rotate(0deg)'
+      });
+      
+      var randomIndex = Math.floor(Math.random() * images.length);
+      $("#floatingHead").attr("src","images/" + images[randomIndex]);
+      $("#floatingVoice").attr("src","audio/" + audios[randomIndex] + ".mp3");
+    });
+  }
+  
+  
   
 
 }
